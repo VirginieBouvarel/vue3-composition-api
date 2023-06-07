20d9c5bacf06161d1c0c4b3f8a50b9fa373c4794
Virginie <vb@ikimo9.com>
2023-06-07 10:20:25 +0200

    App + YummyMeal

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{  name }}</h1>
+  <input type="text" v-model="name">
+  <button @click="placeOrder">Place Order</button>
+  <YummyMeal name="Hamburger" :price="5" @addToCart="addItemToCart"/>
 </template>
+
+<script>
+import YummyMeal from './components/YummyMeal.vue';
+import { ref } from 'vue';
+
+export default {
+  components: { YummyMeal},
+  setup() {
+    const name = ref('The Snazzy Burger');
+    name.value = 'Hello from the setup function';
+    console.log("%c in setup --name.value :","color: yellow", name.value);
+
+    const placeOrder = () => alert("You're order has been placed!");
+    const addItemToCart = (item) => alert(`One ${item} added to the cart!`);
+
+    return { name, placeOrder, addItemToCart };
+  },
+  created() {
+    this.name = 'Hello from the created hook !';
+    console.log("%c in created --name :","color: yellow", this.name);
+
+  }
+}
+</script>
 
 <style scoped>
 .logo {
---
+++ src/components/YummyMeal.vue
@@ -0,0 +1,21 @@
+<template>
+  <p><strong>{{  name }}</strong></p>
+  <button @click="addToCart">Add to Cart</button>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    price: Number,
+  },
+  setup(props, { attrs, slots, emit }) {
+    console.log(props.name);
+    console.log(props.price);
+
+    const addToCart = () => emit("addToCart", props.name);
+
+    return { addToCart };
+  }
+}
+</script>
