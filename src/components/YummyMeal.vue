a0ede37bbe04f3fb60ed804591d33092cbdc8743
Virginie <vb@ikimo9.com>
2023-06-10 07:49:08 +0200

    ajout computed

--- src/components/YummyMeal.vue
+++ src/components/YummyMeal.vue
@@ -1,9 +1,14 @@
 <template>
-  <p><strong>{{  name }}</strong></p>
+  <p>
+    <strong>{{ name }}</strong>
+    {{ pricePretty }}
+  </p>
   <button @click="addToCart">Add to Cart</button>
 </template>
 
 <script>
+import { computed } from 'vue';
+
 export default {
   props: {
     name: String,
@@ -11,7 +16,9 @@ export default {
   },
   setup(props, { emit }) {
     const addToCart = () => emit("addToCart", props.name);
-    return { addToCart };
+    const pricePretty = computed(() => `$${props.price.toFixed(2)}`);
+
+    return { addToCart, pricePretty };
   }
 }
 </script>
