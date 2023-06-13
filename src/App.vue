ac79936ffe8f624e7e6d583987443d95df10a016
Virginie <vb@ikimo9.com>
2023-06-13 09:54:13 +0200

    ajout watch

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,36 @@
   <h1>{{  name }}</h1>
   <input type="text" v-model="name">
   <button @click="placeOrder">Place Order</button>
-  <YummyMeal :name="meal.name" :price="meal.price" @addToCart="addItemToCart"/>
+  <button @click="removeWatcher">Hide cart Alerts</button>
+  <YummyMeal v-for="meal in meals" :name="meal.name" :price="meal.price" @addToCart="addItemToCart"/>
   
 </template>
 
 <script>
 import YummyMeal from './components/YummyMeal.vue';
-import { ref, reactive } from 'vue';
+import { ref, reactive, watch } from 'vue';
 
 export default {
   components: { YummyMeal},
   setup() {
     const name = ref('The Snazzy Burger');
+    const cart = reactive([]);
+    const meal = reactive({ name: "Hambuger", price: 5 });
+    const meals = reactive([
+      { name: "Hambuger", price: 5 },
+      { name: "Cheeseburger", price: 6 },
+      { name: "Impossible Burger", price: 7 },
+      { name: "Fries", price: 2 },
+    ]);
 
-    const meal = reactive({name: "Hambuger", price: 5});
-    
     const placeOrder = () => alert("You're order has been placed!");
-    const addItemToCart = (item) => alert(`One ${item} added to the cart!`);
+    const addItemToCart = (item) => cart.push(item);
 
-    return { name, meal, placeOrder, addItemToCart };
+    const removeWatcher = watch(() => [...cart], (newValue, oldValue) => 
+      alert(newValue.join("\\n"))
+    );
+
+    return { name, meal, meals, placeOrder, addItemToCart, removeWatcher };
   },
 }
 </script>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
